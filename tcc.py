12fd6c1f3d36a1b5180a4cf7b59797c4eba05221
Dan Kirkham <Daniel.A.Kirkham@gmail.com>
2018-12-11 21:51:12 -0600

    Got test script working.

--- tcc.py
+++ tcc.py
@@ -2,23 +2,33 @@
 
 import argparse
 import re
+import os
 from c_compiler.lexer import lex
 from c_compiler.parser import parse
 from c_compiler.generator import generate
 
-def determine_filename(args):
+def determine_assembly_filename(args):
     """Determine default file name for assembly output."""
+    # Swap .c with .S
+    m = re.search("(.*)\.[cC]", args.source_file)
+    if m:
+        return m.group(1) + '.S'
+
+    # Last resort, append .S
+    return args.source_file + '.S'
+
+def determine_executable_filename(args):
+    """Determine default file name for ELF output."""
     if args.o:
         return args.o
 
     # Swap .c with .S
     m = re.search("(.*)\.[cC]", args.source_file)
     if m:
-        # return m.group(1) + '.S'
         return m.group(1)
 
     # Last resort, append .S
-    return args.source_file + '.S'
+    return 'a.out'
 
 def main():
     parser = argparse.ArgumentParser()
@@ -30,9 +40,13 @@ def main():
     with open(args.source_file, 'r') as f:
         tokens = lex(f.read())
 
-    print(tokens)
     ast = parse(tokens)
-    generate(ast, determine_filename(args))
+
+    asm_file = determine_assembly_filename(args)
+    generate(ast, asm_file)
+
+    os.system('gcc -m32 {} -o {}'.format(asm_file, determine_executable_filename(args)))
+    os.system('rm {}'.format(asm_file))
 
 if __name__ == "__main__":
     main()
