98364d4a821acf1e26213bf1f7bdf856e63b8c13
Dan Kirkham <Daniel.A.Kirkham@gmail.com>
2018-12-13 23:00:26 -0600

    Added impoved test script.

--- Makefile
+++ Makefile
@@ -1,4 +1,4 @@
 .PHONY: test
 test:
 	/usr/bin/env python3 -m unittest discover
-	cd write_a_c_compiler ; ./test_compiler.sh ../tcc.py 1
+	cd write_a_c_compiler ; ./test_compiler.sh ../tcc.sh 1
---
+++ tcc.sh
@@ -0,0 +1,16 @@
+#!/usr/bin/env bash
+
+DIR=write_a_c_compiler
+EXE_FILE=${1%.*}
+ASM_FILE=${EXE_FILE}.S
+
+# Compile
+cd ..
+python3.5 -m c_compiler $DIR/$1
+
+# Assemble
+gcc -m32 $DIR/$ASM_FILE -o $DIR/$EXE_FILE
+rm $DIR/$ASM_FILE
+
+# Make Executeable
+chmod +x $DIR/$EXE_FILE
