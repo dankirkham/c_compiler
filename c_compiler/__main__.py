1ad81e253c65c78bf8bde00174969b017f96cbff
Dan Kirkham <Daniel.A.Kirkham@gmail.com>
2018-12-13 22:45:13 -0600

    Revert __main__ changes.

--- c_compiler/__main__.py
+++ c_compiler/__main__.py
@@ -1,14 +1,14 @@
-#!/usr/bin/env python3
-
 import argparse
 import re
-import os
 from c_compiler.lexer import lex
 from c_compiler.parser import parse
 from c_compiler.generator import generate
 
-def determine_assembly_filename(args):
+def determine_filename(args):
     """Determine default file name for assembly output."""
+    if args.o:
+        return args.o
+
     # Swap .c with .S
     m = re.search("(.*)\.[cC]", args.source_file)
     if m:
@@ -16,19 +16,6 @@ def determine_assembly_filename(args):
 
     # Last resort, append .S
     return args.source_file + '.S'
-
-def determine_executable_filename(args):
-    """Determine default file name for ELF output."""
-    if args.o:
-        return args.o
-
-    # Swap .c with .S
-    m = re.search("(.*)\.[cC]", args.source_file)
-    if m:
-        return m.group(1)
-
-    # Last resort, append .S
-    return 'a.out'
 
 def main():
     parser = argparse.ArgumentParser()
@@ -40,13 +27,9 @@ def main():
     with open(args.source_file, 'r') as f:
         tokens = lex(f.read())
 
+    print(tokens)
     ast = parse(tokens)
-
-    asm_file = determine_assembly_filename(args)
-    generate(ast, asm_file)
-
-    os.system('gcc -m32 {} -o {}'.format(asm_file, determine_executable_filename(args)))
-    os.system('rm {}'.format(asm_file))
+    generate(ast, determine_filename(args))
 
 if __name__ == "__main__":
     main()
