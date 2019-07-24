707c9dc7b5949889566ea8deb043d2269e47a0c3
Dan Kirkham <Daniel.A.Kirkham@gmail.com>
2019-07-24 11:10:22 -0500

    64-bit machine language. Swapped Term and Factor operations.

--- c_compiler/generator.py
+++ c_compiler/generator.py
@@ -5,17 +5,17 @@ def generate_factor(factor, f):
         generate_expression(factor.expression, f)
 
         if factor.operator == 'negation':
-            f.write("neg     %eax\n")
+            f.write("neg     %rax\n")
         elif factor.operator == 'bitwise_complement':
-            f.write("not     %eax\n")
+            f.write("not     %rax\n")
         elif factor.operator == 'logical_negation':
-            f.write("cmpl    $0, %eax\n")
-            f.write("movl    $0, %eax\n")
+            f.write("cmpq    $0, %rax\n")
+            f.write("movq    $0, %rax\n")
             f.write("sete    %al\n")
     elif isinstance(factor, ast_data_structures.Expression):
         generate_expression(factor, f)
     elif isinstance(factor, ast_data_structures.Constant):
-        f.write("movl    ${}, %eax\n".format(factor.integer))
+        f.write("movq    ${}, %rax\n".format(factor.integer))
 
 def generate_term(term, f):
     operations = iter(term.operations)
@@ -30,11 +30,17 @@ def generate_term(term, f):
             pass
 
     for operation in term.operations:
-        f.write("pop     %rcx\n")
-        if operation == 'negation':
-            # TODO: Tighten up subtraction
-            f.write("neg     %eax\n")
-        f.write("addl    %ecx, %eax\n")
+        if operation == 'multiplication_operator':
+            f.write("pop     %rcx\n")
+            f.write("imul    %rcx, %rax\n")
+        elif operation == 'division_operator':
+            # TODO: Tighten up
+            f.write("pop     %rdx\n") # e1 to RDX
+            f.write("movq    %rax, %rcx\n") # e2 to RCX
+            f.write("movq    %rdx, %rax\n") # e1 to RAX
+            f.write("movq    $0, %rdx\n") # Zero RDX
+            # f.write("cqto\n")
+            f.write("idivq   %rcx\n")
 
 def generate_expression(expression, f):
     operations = iter(expression.operations)
@@ -48,17 +54,12 @@ def generate_expression(expression, f):
         except StopIteration:
             pass
 
-    for operation in term.operations:
-        if operation == 'multiplication_operator':
-            f.write("pop     %rcx\n")
-            f.write("imul    %ecx, %eax\n")
-        elif operation == 'division_operator':
-            # TODO: Tighten up
-            f.write("pop     %rdx\n") # e1 to EDX
-            f.write("movl    %eax, %ecx\n") # e2 to ECX
-            f.write("movl    %edx, %eax\n") # e1 to EAX
-            f.write("movl    $0, %edx\n") # Zero EDX
-            f.write("idivl   %ecx\n")
+    for operation in expression.operations:
+        f.write("pop     %rcx\n")
+        if operation == 'negation':
+            # TODO: Tighten up subtraction
+            f.write("neg     %rax\n")
+        f.write("addq    %rcx, %rax\n")
 
 def generate_statement(statement, f):
     # TODO: Currently assuming return statement
