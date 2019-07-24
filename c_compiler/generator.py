df526511faf3fec7e53b1028f395dfe349d8785d
Dan Kirkham <Daniel.A.Kirkham@gmail.com>
2019-07-24 10:33:34 -0500

    WSL portability.

--- c_compiler/generator.py
+++ c_compiler/generator.py
@@ -70,8 +70,13 @@ def generate_statement(statement, f):
 def generate_function(function, f):
     label = function.name
 
+    # macOS
     f.write(".globl _{}\n".format(label))
     f.write("_{}:\n".format(label))
+
+    # Debian WSL
+    f.write(".globl {}\n".format(label))
+    f.write("{}:\n".format(label))
 
     # TODO: Generate all body statements
     generate_statement(function.statement, f)
